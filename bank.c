e9649f925b9a2a057f529826ba554cd3d73095ac
Symrak43 <symrak43@yandex.ru>
2017-03-16 20:43:48 +0700

    Made input/output

---
+++ bank.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+  
+int main(){
+
+    int sum, data, vihod;
+    printf("Сумма вклада: ");
+    scanf("%d", &sum);
+    printf("Срок вклада: ");
+    scanf("%d", &data);
+
+    return 0;
+};
