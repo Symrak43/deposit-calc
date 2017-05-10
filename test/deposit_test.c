a6a7e9528f3de46fe4ab2853f91176269154fba5
Symrak43 <Symrak43@yandex.ru>
2017-05-11 00:21:38 +0700

    Names tests

--- test/deposit_test.c
+++ test/deposit_test.c
@@ -1,55 +1,49 @@
 #include "deposit.h"
 #include "ctest.h"
 
-CTEST(less100, test1) {
+CTEST(sumLess100k, dateBetween0and30) {
 	int result = vklad(1000,30);
 	int extended = 900;
 	ASSERT_EQUAL(result, extended);
 }
 
-CTEST(less100, test2) {
+CTEST(sumLess100k, dateBetween31and120) {
 	int result = vklad(5000,60);
 	int extended = 5100;
 	ASSERT_EQUAL(result, extended);
 }
 
-CTEST(less100, test3) {
+CTEST(sumLess100k, dateBetween121and240) {
 	int result = vklad(15000,175);
 	int extended = 15900;
 	ASSERT_EQUAL(result, extended);
 }
 
-CTEST(less100, test4) {
+CTEST(sumLess100k, dateBetween241and365) {
 	int result = vklad(50000,300);
 	int extended = 56000;
 	ASSERT_EQUAL(result, extended);
 }
 
-CTEST(less100, test5) {
-	int result = vklad(99999,0);
-	int extended = 89999;
-	ASSERT_EQUAL(result, extended);
-}
-
-CTEST(more100, test1) {
+CTEST(sumMore100, dateBetween0and30) {
 	int result = vklad(100000,30);
 	int extended = 90000;
 	ASSERT_EQUAL(result, extended);
 }
 
-CTEST(more100, test2) {
+CTEST(sumMore100, dateBetween31and120) {
 	int result = vklad(150000,85);
 	int extended = 154500;
 	ASSERT_EQUAL(result, extended);
 }
 
-CTEST(more100, test3) {
+CTEST(sumMore100, dateBetween121and240) {
 	int result = vklad(225000,240);
 	int extended = 243000;
 	ASSERT_EQUAL(result, extended);
 }
 
-CTEST(more100, test4) {
+CTEST(sumMore100, dateBetween241and365) {
 	int result = vklad(374000,365);
 	int extended = 430100;
 	ASSERT_EQUAL(result, extended);
--- test/validation_test.c
+++ test/validation_test.c
@@ -1,37 +1,37 @@
 #include "deposit.h"
 #include "ctest.h"
 
-CTEST(validation, test1) {
+CTEST(validationInput, Sum0Date0) {
 	int result = errore(0, 0);
 	int extended = 0;
 	ASSERT_EQUAL(result, extended);
 }
 
-CTEST(validation, test2) {
+CTEST(validationInput, Sum0dateLessNull) {
 	int result = errore(0, -1);
 	int extended = 0;
 	ASSERT_EQUAL(result, extended);
 }
 
-CTEST(validation, test3) {
+CTEST(validationInput, Sum10000date366_) {
 	int result = errore(10000, 366);
 	int extended = 0;
 	ASSERT_EQUAL(result, extended);
 }
 
-CTEST(validation, test4) {
+CTEST(validationInput, Sum10000dateLessNull) {
 	int result = errore(10000, -1);
 	int extended = 0;
 	ASSERT_EQUAL(result, extended);
 }
 
-CTEST(validation, test5) {
+CTEST(validationInput, Sum10000date0) {
 	int result = errore(10000,0);
 	int extended = 1;
 	ASSERT_EQUAL(result, extended);
 }
 
-CTEST(validation, test6) {
+CTEST(validationInput, Sum10000date365) {
 	int result = errore(10000,365);
 	int extended = 1;
 	ASSERT_EQUAL(result, extended);
